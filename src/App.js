347f2cf30105743fc62e3e5b815fecf0e85cc49b
luizhen1 <oluizhen1@gmail.com>
2025-02-14 12:34:52 -0300

    add: deploy workflow

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Sobre from "./pages/sobre/Sobre";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import Parcerias from './pages/parcerias/Parcerias';
+import Carreiras from './pages/carreiras/Carreiras';
+import Dados from './pages/dados/Dados';
+import Login  from './pages/loginpage/login';
+import Pacotes  from './pages/pacotes/pacotes';
+
+function App() {
+  return (
+    <BrowserRouter basename="/DestinoCerto--UniFil">
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/parcerias' element={<Parcerias />} />
+        <Route path='/sobre' element={<Sobre />} />
+        <Route path='/carreiras' element={<Carreiras />} />
+        <Route path='/infos' element={<Dados />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/pacotes' element={<Pacotes />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
